e89a7e51b878878f4dc73eea95521a176d06a99f
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-08-03 18:01:42 -0300

    model product

---
+++ src/main/java/com/aleiku/inyecciondependencias/practicados/springapp/models/Product.java
@@ -0,0 +1,46 @@
+package com.aleiku.inyecciondependencias.practicados.springapp.models;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Long price;
+
+
+    //contructor methods
+
+    public Product(Long id, String name, Long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(){};
+
+
+    //getter and setter methods
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getPrice() {
+        return price;
+    }
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+
+    
+
+}
