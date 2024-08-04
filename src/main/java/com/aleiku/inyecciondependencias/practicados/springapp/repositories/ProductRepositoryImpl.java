94eca804484d3f555617e814f7bc3cdd4f9226f5
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-08-04 01:23:12 -0300

    Interfaces Repository y Service

---
+++ src/main/java/com/aleiku/inyecciondependencias/practicados/springapp/repositories/ProductRepositoryImpl.java
@@ -0,0 +1,48 @@
+package com.aleiku.inyecciondependencias.practicados.springapp.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.aleiku.inyecciondependencias.practicados.springapp.models.Product;
+
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private List <Product> products;
+
+
+    public ProductRepositoryImpl() {
+        this.products = Arrays.asList(
+            new Product(1L,"Nintendo Switch",300L),
+            new Product(2L,"The Legend Of Zelda: Tears of the Kingdom",70L),
+            new Product(3L,"Splatoon 3",70L),
+            new Product(4L,"Animal Crossing: New Horizon",50L)
+        );
+    }
+
+    @Override
+    public List<Product> findAll(){
+        return products;
+
+    }
+
+    @Override
+    public Product findById(Long id){
+
+        // var findProduct = products.stream().filter(p->p.getId().equals(id)).findFirst();
+
+        // if (findProduct.isPresent()){
+        //     return findProduct.get();
+
+        // }else{
+        //     return null;
+        // }
+        
+        return products.stream().filter(p->p.getId().equals(id)).findFirst().orElse(null);
+
+
+
+    }
+
+    
+
+}
---
+++ src/main/java/com/aleiku/inyecciondependencias/practicados/springapp/service/ProductServiceImpl.java
@@ -0,0 +1,29 @@
+package com.aleiku.inyecciondependencias.practicados.springapp.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.aleiku.inyecciondependencias.practicados.springapp.models.Product;
+import com.aleiku.inyecciondependencias.practicados.springapp.repositories.ProductRepository;
+
+public class ProductService {// Se puede hacer logica de negocio
+
+    private ProductRepository productRepository = new ProductRepository();
+
+    public List <Product> findAll(){
+        return productRepository.findAll().stream().map(product -> {
+
+            Double priceTax = product.getPrice() * 1.25d;
+            Product newProduct = (Product)product.clone();
+            newProduct.setPrice(priceTax.longValue());
+            return newProduct;//devuelvo el producto modificado
+
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id){
+
+        return productRepository.findById(id);
+    }
+
+}
